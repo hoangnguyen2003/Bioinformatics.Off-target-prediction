d249339d7909ee1703695178578cfe031f39ed58
hoangnguyen2003 <ninjadoc0152@gmail.com>
2024-05-17 14:25:00 +0700

    dataset and encoding

--- model/dataset_utils.py
+++ model/dataset_utils.py
@@ -1,15 +1,47 @@
 import numpy as np
 from numpy import loadtxt
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class Dataset:
     def __init__(self, dataset_dir):
         self.dataset_dir = dataset_dir
+
+        self.file_names = ['SITE-Seq_offTarget_wholeDataset']
+        self.file_column_dict = {
+            'SITE-Seq_offTarget_wholeDataset': ('on_seq', 'off_seq', 'reads', 'on_seq')
+        }
+
         self.code_dict = {
             'A': [1, 0, 0, 0],
             'T': [0, 1, 0, 0],
             'G': [0, 0, 1, 0],
             'C': [0, 0, 0, 1],
         }
+
+    def load_data(self, filename):
+        columns = self.file_column_dict[filename]
+        data = pd.read_csv(self.dataset_dir)
+
+        sgRNAs = data[columns[0]]
+        DNAs = data[columns[1]]
+        labels = data[columns[2]]
+        sgRNA_types = data[columns[3]]
+
+        sgRNAs = sgRNAs.apply(lambda sgRNA: sgRNA.upper())
+        DNAs = DNAs.apply(lambda DNA: DNA.upper())
+        labels = labels.apply(lambda label: int(label != 0))
+
+        sgRNAs_new = []
+        for index, sgRNA in enumerate(sgRNAs):
+            sgRNA = list(sgRNA)
+            sgRNA[-3] = DNAs[index][-3]
+            sgRNAs_new.append(''.join(sgRNA))
+
+        sgRNAs = pd.Series(sgRNAs_new)
+        data = pd.DataFrame.from_dict({'sgRNAs':sgRNAs, 'DNAs':DNAs, 'labels':labels})
+        return data[data.apply(lambda row: 'N' not in list(row['DNAs']), axis = 1)]
 
     def preprocess_function(self, guide_seq, off_seq):
         gRNA_list = list(guide_seq)
@@ -19,12 +51,13 @@ class Dataset:
             gRNA_base_code = self.code_dict[gRNA_list[i]]
             DNA_based_code = self.code_dict[off_list[i]]
             pair_code.append(list(np.bitwise_or(gRNA_base_code, DNA_based_code)))
-        
         return np.array(pair_code).reshape(1, 1, 23, 4)
 
     def get_final_ds(self):
-        dataset = loadtxt(self.dataset_dir, delimiter=',')
-        # X = dataset[:,0:8]
-        # y = dataset[:,8]
-        # return dataloader
-        return dataset
+        dataset = self.load_data(os.path.splitext(
+            os.path.basename(self.dataset_dir))[0])
+        datset_encodings = np.array(train.apply(
+            lambda row: Dataset("datasets\SITE-Seq_offTarget_wholeDataset.csv").preprocess_function(
+                row['sgRNAs'], row['DNAs']), axis = 1).to_list())
+        train, test = train_test_split(datset_encodings, test_size=0.2, random_state=42)
+        return train, test
