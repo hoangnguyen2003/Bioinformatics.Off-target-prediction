63e4a5c356f4147f9ecf9bd00366c1a487c7b721
hoangnguyen2003 <ninjadoc0152@gmail.com>
2024-05-18 16:05:42 +0700

    fix bug

--- model/train.py
+++ model/train.py
@@ -15,7 +15,8 @@ def main(args):
     # off_target_prediction.do_all()
     off_target_prediction.get_data()
     for a in off_target_prediction.X_test:
-        print(a.shape)
+        if a.shape != (1, 23, 4):
+            print(a.shape)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
