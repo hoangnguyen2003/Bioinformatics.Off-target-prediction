9e0104228fdb39143a47bb898c6ef641c980d8ac
hoangnguyen2003 <ninjadoc0152@gmail.com>
2024-05-29 01:28:54 +0700

    test

--- model/dataset_utils.py
+++ model/dataset_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import pandas as pd
+import xlrd
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
@@ -10,7 +11,9 @@ class Dataset:
     def __init__(self, dataset_dir):
         self.dataset_dir = dataset_dir
 
-        self.file_names = ['SITE-Seq_offTarget_wholeDataset', 'CIRCLE_seq_10gRNA_wholeDataset']
+        self.file_names = ['SITE-Seq_offTarget_wholeDataset',
+                           'CIRCLE_seq_10gRNA_wholeDataset',
+                           'off_data_twoset']
         self.file_column_dict = {
             'SITE-Seq_offTarget_wholeDataset': ('on_seq', 'off_seq', 'reads', 'on_seq'),
             'CIRCLE_seq_10gRNA_wholeDataset': ('sgRNA_seq', 'off_seq', 'label', 'sgRNA_type')
@@ -26,26 +29,40 @@ class Dataset:
         }
 
     def load_data(self, filename):
-        columns = self.file_column_dict[filename]
-        data = pd.read_csv(self.dataset_dir)
+        if filename != "off_data_twoset":
+            columns = self.file_column_dict[filename]
+            data = pd.read_csv(self.dataset_dir)
 
-        sgRNAs = data[columns[0]]
-        DNAs = data[columns[1]]
-        labels = data[columns[2]]
+            sgRNAs = data[columns[0]]
+            DNAs = data[columns[1]]
+            labels = data[columns[2]]
 
-        sgRNAs = sgRNAs.apply(lambda sgRNA: sgRNA.upper())
-        DNAs = DNAs.apply(lambda DNA: DNA.upper())
-        labels = labels.apply(lambda label: int(label != 0))
+            sgRNAs = sgRNAs.apply(lambda sgRNA: sgRNA.upper())
+            DNAs = DNAs.apply(lambda DNA: DNA.upper())
+            labels = labels.apply(lambda label: int(label != 0))
 
-        sgRNAs_new = []
-        for index, sgRNA in enumerate(sgRNAs):
-            sgRNA = list(sgRNA)
-            sgRNA[-3] = DNAs[index][-3]
-            sgRNAs_new.append(''.join(sgRNA))
+            sgRNAs_new = []
+            for index, sgRNA in enumerate(sgRNAs):
+                sgRNA = list(sgRNA)
+                sgRNA[-3] = DNAs[index][-3]
+                sgRNAs_new.append(''.join(sgRNA))
 
-        sgRNAs = pd.Series(sgRNAs_new)
-        data = pd.DataFrame.from_dict({'sgRNAs':sgRNAs, 'DNAs':DNAs, 'labels':labels})
-        return data[data.apply(lambda row: 'N' not in list(row['DNAs']), axis = 1)]
+            sgRNAs = pd.Series(sgRNAs_new)
+            data = pd.DataFrame.from_dict({'sgRNAs':sgRNAs, 'DNAs':DNAs, 'labels':labels})
+            return data[data.apply(lambda row: 'N' not in list(row['DNAs']), axis = 1)]
+        else:
+            InputFile = xlrd.open_workbook(self.dataset_dir)
+            sheet_hek293t = InputFile.sheet_by_name('hek293t')
+            hek_sgRNA_list = sheet_hek293t.col_values(1)
+            hek_DNA_list = sheet_hek293t.col_values(2)
+            hek_labels_list = sheet_hek293t.col_values(3)
+            sheet_K562 = InputFile.sheet_by_name('K562')
+            K562_sgRNA_list = sheet_K562.col_values(1)
+            K562_DNA_list = sheet_K562.col_values(2)
+            K562_labels_list = sheet_K562.col_values(3)
+            print(K562_DNA_list)
+            print(K562_labels_list)
+            exit()
 
     def preprocess_function(self, guide_seq, off_seq):
         gRNA_list = list(guide_seq)
